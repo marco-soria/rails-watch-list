36628b9ac21b41932dbbc2db7e8c9783894ef6f0
Marco Soria <marco.soria@pucp.edu.pe>
2023-11-20 16:04:30 -0500

    Add dotenv-rails

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem "stimulus-rails"
 gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
-# gem "redis", "~> 4.0"
+gem "redis", "~> 4.0"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
@@ -56,6 +56,7 @@ gem "simple_form"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "dotenv-rails"
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+# # URL de la API con el proxy REVISAR CODIGO
+# api_url = 'http://tmdb.lewagon.com/movie/top_rated?limit=12'
+
+# # Obtén los datos de la API
+# movies_serialized = URI.open(api_url).read
+# movies = JSON.parse(movies_serialized)['results']
+
+# # Itera sobre las películas y crea registros en la base de datos
+# movies.first(12).each do |movie|
+#   Movie.create(
+#     title: movie['title'],
+#     overview: movie['overview'],
+#     poster_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}",
+#     rating: movie['vote_average']
+#   )
+# end
+
+# puts 'Seed finished'
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "#{base_poster_url}#{movie["backdrop_path"]}",
+      rating: movie["vote_average"]
+    )
+  end
+end
+puts "Movies created"
